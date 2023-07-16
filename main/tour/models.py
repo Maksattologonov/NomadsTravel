66d5fc4f5915b79846fad7069e4d4719fda60e9e
makstt <maksattologonovn@gmail.com>
2023-07-16 15:56:13 +0600

    start adding files

--- main/tour/models.py
+++ main/tour/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_('Название сайта'))
+    city = models.CharField(max_length=255, verbose_name=_('Город'))
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_('Название города'))
+    region = models.CharField()
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_('Название области'))
+    description = models.TextField(verbose_name=_('Описание'))
+    image = models.FileField(upload_to='regions')
